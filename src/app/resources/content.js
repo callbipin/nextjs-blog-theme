48d258cdbc7a9f02b2d76fd41179cf70749b7056
Bipin Rimal <bri@logpoint.com>
2024-12-28 16:30:30 +0545

    Updated files

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,256 +1,234 @@
 import { InlineCode } from "@/once-ui/components";
 
 const person = {
-    firstName: 'Selene',
-    lastName:  'Yu',
-    get name() {
-        return `${this.firstName} ${this.lastName}`;
-    },
-    role:      'Design Engineer',
-    avatar:    '/images/avatar.jpg',
-    location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-    languages: ['English', 'Bahasa']  // optional: Leave the array empty if you don't want to display languages
-}
+  firstName: "Bipin",
+  lastName: "Rimal",
+  get name() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  role: "Technical Writer",
+  avatar: "/images/avatar.jpg",
+  location: "Asia/Kathmandu", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "Nepali"], // optional: Leave the array empty if you don't want to display languages
+};
 
 const newsletter = {
-    display: true,
-    title: <>Subscribe to {person.firstName}'s Newsletter</>,
-    description: <>I occasionally write about design, technology, and share thoughts on the intersection of creativity and engineering.</>
-}
+  display: true,
+  title: <>Have any questions?</>,
+  description: (
+    <>
+      I am a person who loves communicating with people. If you have any
+      querries, you can reach out to me through email.
+    </>
+  ),
+};
 
 const social = [
-    // Links are automatically displayed.
-    // Import new icons in /once-ui/icons.ts
-    {
-        name: 'GitHub',
-        icon: 'github',
-        link: 'https://github.com/once-ui-system/nextjs-starter',
-    },
-    {
-        name: 'LinkedIn',
-        icon: 'linkedin',
-        link: 'https://www.linkedin.com/company/once-ui/',
-    },
-    {
-        name: 'X',
-        icon: 'x',
-        link: '',
-    },
-    {
-        name: 'Email',
-        icon: 'email',
-        link: 'mailto:example@gmail.com',
-    },
-]
+  // Links are automatically displayed.
+  // Import new icons in /once-ui/icons.ts
+  {
+    name: "GitHub",
+    icon: "github",
+    link: "https://github.com/BipinRimal314",
+  },
+  {
+    name: "LinkedIn",
+    icon: "linkedin",
+    link: "https://www.linkedin.com/in/bipinrimal314/",
+  },
+  {
+    name: "X",
+    icon: "x",
+    link: "https://x.com/Lamir314",
+  },
+  {
+    name: "Email",
+    icon: "email",
+    link: "mailto:bipinrimal314@gmail.com",
+  },
+];
 
 const home = {
-    label: 'Home',
-    title: `${person.name}'s Portfolio`,
-    description: `Portfolio website showcasing my work as a ${person.role}`,
-    headline: <>Design engineer and builder</>,
-    subline: <>I'm Selene, a design engineer at <InlineCode>FLY</InlineCode>, where I craft intuitive<br/> user experiences. After hours, I build my own projects.</>
-}
+  label: "Home",
+  title: `${person.name}'s Portfolio`,
+  description: `Portfolio website showcasing my work as a ${person.role}`,
+  headline: <>Writer and Generalist</>,
+  subline: (
+    <>
+      I'm Bipin, a writer, public speaker, and an overall generalist.
+      <br />
+      After hours, I make videos about communication, life, liberty, and the
+      pursuit of sausages.
+    </>
+  ),
+};
 
 const about = {
-    label: 'About',
-    title: 'About me',
-    description: `Meet ${person.name}, ${person.role} from ${person.location}`,
-    tableOfContent: {
-        display: true,
-        subItems: false
-    },
-    avatar: {
-        display: true
-    },
-    calendar: {
-        display: true,
-        link: 'https://cal.com'
-    },
-    intro: {
-        display: true,
-        title: 'Introduction',
-        description: <>Selene is a Jakarta-based design engineer with a passion for transforming complex challenges into simple, elegant design solutions. Her work spans digital interfaces, interactive experiences, and the convergence of design and technology.</>
-    },
-    work: {
-        display: true, // set to false to hide this section
-        title: 'Work Experience',
-        experiences: [
-            {
-                company: 'FLY',
-                timeframe: '2022 - Present',
-                role: 'Senior Design Engineer',
-                achievements: [
-                    <>Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user engagement and 30% faster load times.</>,
-                    <>Spearheaded the integration of AI tools into design workflows, enabling designers to iterate 50% faster.</>
-                ],
-                images: [ // optional: leave the array empty if you don't want to display images
-                    {
-                        src: '/images/projects/project-01/cover-01.jpg',
-                        alt: 'Once UI Project',
-                        width: 16,
-                        height: 9
-                    }
-                ]
-            },
-            {
-                company: 'Creativ3',
-                timeframe: '2018 - 2022',
-                role: 'Lead Designer',
-                achievements: [
-                    <>Developed a design system that unified the brand across multiple platforms, improving design consistency by 40%.</>,
-                    <>Led a cross-functional team to launch a new product line, contributing to a 15% increase in overall company revenue.</>
-                ],
-                images: [ ]
-            }
-        ]
-    },
-    studies: {
-        display: true, // set to false to hide this section
-        title: 'Studies',
-        institutions: [
-            {
-                name: 'University of Jakarta',
-                description: <>Studied software engineering.</>,
-            },
-            {
-                name: 'Build the Future',
-                description: <>Studied online marketing and personal branding.</>,
-            }
-        ]
-    },
-    technical: {
-        display: true, // set to false to hide this section
-        title: 'Technical skills',
-        skills: [
-            {
-                title: 'Figma',
-                description: <>Able to prototype in Figma with Once UI with unnatural speed.</>,
-                // optional: leave the array empty if you don't want to display images
-                images: [
-                    {
-                        src: '/images/projects/project-01/cover-02.jpg',
-                        alt: 'Project image',
-                        width: 16,
-                        height: 9
-                    },
-                    {
-                        src: '/images/projects/project-01/cover-03.jpg',
-                        alt: 'Project image',
-                        width: 16,
-                        height: 9
-                    },
-                ]
-            },
-            {
-                title: 'Next.js',
-                description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
-                // optional: leave the array empty if you don't want to display images
-                images: [
-                    {
-                        src: '/images/projects/project-01/cover-04.jpg',
-                        alt: 'Project image',
-                        width: 16,
-                        height: 9
-                    },
-                ]
-            }
-        ]
-    }
-}
+  label: "About",
+  title: "About me",
+  description: `Meet ${person.name}, ${person.role} from ${person.location}`,
+  tableOfContent: {
+    display: true,
+    subItems: false,
+  },
+  avatar: {
+    display: true,
+  },
+  calendar: {
+    display: true,
+    link: "https://cal.com",
+  },
+  intro: {
+    display: true,
+    title: "Introduction",
+    description: (
+      <>
+        I am a Tech Support Specialist with 4+ years experience in Technical
+        Troubleshooting and Documentation. <br />
+        <br />
+        As the bridge between Product and Customer, I create comprehensive
+        guides that act as a roadmap customers can follow to reach their goals.
+        My in-depth analysis of the customer feedback addresses the key pain
+        points of the customers.
+        <br />
+        <br />
+        Additionally, I am a huge fan of telling and listening to stories. In my
+        free time, you will find me in Toastmasters around Kathmandu Valley,
+        listening to people's stories and smiling. If you are ever so lucky, you
+        may even get to see me get up on stage doing my best to make people
+        laugh.
+      </>
+    ),
+  },
+  work: {
+    display: true, // set to false to hide this section
+    title: "Work Experience",
+    experiences: [
+      {
+        company: "Logpoint",
+        timeframe: "Feb 2023 - Present",
+        role: "Technical Writer",
+        achievements: [
+          <>
+            Addressed technical inquiries of customers via Zendesk, providing
+            timely and effective customer support.
+          </>,
+          <>
+            Collaborated with the Product team (Product Owners, Engineering
+            Managers, Developers, and QA) to understand new features and develop
+            comprehensive documentation.
+          </>,
+          <>
+            Added correct technical details about enhancements, bug fixes, and
+            vulnerability fixes in the release notes.
+          </>,
+          <>
+            Collaborated with the SecOps team to create Product Vulnerability
+            Reports and Security Testing Guides, ensuring product compliance
+            with customer requirements.
+          </>,
+        ],
+        images: [
+          // optional: leave the array empty if you don't want to display image
+        ],
+      },
+      {
+        company: "ComplianceQuest",
+        timeframe: " Feb 2020 - Jan 2023",
+        role: "Technical Writer",
+        achievements: [
+          <>
+            Authored Installation Qualification and Operational Qualification
+            documentation for Salesforce CRM products, ensuring compliance with
+            industry standards.
+          </>,
+          <>
+            Performed regression testing, achieving a 100% success of test
+            cases, demonstrating meticulous attention to detail and a commitment
+            to delivering high-quality products.
+          </>,
+        ],
+        images: [],
+      },
+      {
+        company: "Weesho Lapara",
+        timeframe: " Jan 2020 - Feb 2022",
+        role: "Technical Writer",
+        achievements: [
+          <>
+            Provided technical support to customers via Zoom, email, and live
+            chat, demonstrating strong communication skills and a
+            customer-centric approach.
+          </>,
+          <>
+            Onboarded four new clients, creating comprehensive documentation
+            tailored to their needs, showcasing an ability to understand and
+            meet customer requirements.
+          </>,
+        ],
+        images: [],
+      },
+    ],
+  },
+  studies: {
+    display: true, // set to false to hide this section
+    title: "Studies",
+    institutions: [
+      {
+        name: "Coventry Unversity",
+        description: <>MSc Data Science and Computational Intelligence</>,
+      },
+      {
+        name: "Thames International College",
+        description: <>Bachelors in Information Management.</>,
+      },
+    ],
+  },
+  technical: {
+    display: true, // set to false to hide this section
+    title: "Skills",
+    skills: [
+      {
+        title: "Tools & Platforms",
+        description: (
+          <>
+            Proficient in MadCap, Confluence, Jira, Zendesk, Slack, Git, SIEM,
+            SOAR, and Salesforce. Knowledge of Programming languages (NodeJs,
+            Python), programming concepts (RESTAPI, OOP). More skills present in
+            <a href="https://www.salesforce.com/trailblazer/bipinrimal">
+              Trailblazer
+            </a>
+            .
+          </>
+        ),
+        // optional: leave the array empty if you don't want to display images
+        images: [],
+      },
+      {
+        title: "Soft Skills",
+        description: (
+          <>
+            <a href="https://www.goskills.com/Profile/BipinRimal">
+              GoSkills certified
+            </a>{" "}
+            in Customer Service Training and Business Writing. Placed second in
+            International Speech Contest 2023-2024, Area A4, District 41,
+            Toastmasters Internation
+          </>
+        ),
+        // optional: leave the array empty if you don't want to display images
+        images: [],
+      },
+    ],
+  },
+};
 
 const blog = {
-    label: 'Blog',
-    title: 'Writing about design and tech...',
-    description: `Read what ${person.name} has been up to recently`
-    // Create new blog posts by adding a new .mdx file to app/blog/posts
-    // All posts will be listed on the /blog route
-}
+  label: "Blog",
+  title: "Writing about design and tech...",
+  description: `Read what ${person.name} has been up to recently`,
+  // Create new blog posts by adding a new .mdx file to app/blog/posts
+  // All posts will be listed on the /blog route
+};
 
-const work = {
-    label: 'Work',
-    title: 'My projects',
-    description: `Design and dev projects by ${person.name}`
-    // Create new project pages by adding a new .mdx file to app/blog/posts
-    // All projects will be listed on the /home and /work routes
-}
-
-const gallery = {
-    label: 'Gallery',
-    title: 'My photo gallery',
-    description: `A photo collection by ${person.name}`,
-    // Images from https://pexels.com
-    images: [
-        { 
-            src: '/images/gallery/img-01.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
-        },
-        { 
-            src: '/images/gallery/img-02.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-03.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
-        },
-        { 
-            src: '/images/gallery/img-04.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-05.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-06.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
-        },
-        { 
-            src: '/images/gallery/img-07.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-08.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
-        },
-        { 
-            src: '/images/gallery/img-09.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-10.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-11.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
-        },
-        { 
-            src: '/images/gallery/img-12.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-13.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-14.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-    ]
-}
-
-export { person, social, newsletter, home, about, blog, work, gallery };
+export { person, social, newsletter, home, about, blog };
