1755e566cf48511e941ea795d6b0ef9b6b592bca
Bipin Rimal <bri@logpoint.com>
2024-12-28 16:38:08 +0545

    Updated files

--- src/app/resources/renderContent.js
+++ src/app/resources/renderContent.js
@@ -1,22 +1,19 @@
-import { person, newsletter, social, home, about, blog, work, gallery } from './content';
-import { createI18nContent } from './content-i18n';
-import { i18n } from './config';
+import { person, social, home, about, blog } from "./content";
+import { createI18nContent } from "./content-i18n";
+import { i18n } from "./config";
 
 const renderContent = (t) => {
-    if ( i18n ) {
-        return createI18nContent(t);
-    } else {
-        return {
-            person,
-            social,
-            newsletter,
-            home,
-            about,
-            blog,
-            work,
-            gallery
-        }
-    }
+  if (i18n) {
+    return createI18nContent(t);
+  } else {
+    return {
+      person,
+      social,
+      home,
+      about,
+      blog,
+    };
+  }
 };
 
 export { renderContent };
