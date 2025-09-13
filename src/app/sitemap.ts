55da424dc17c314f68502a8ca5d13b6a0cc47c76
org-p0-security-ekline <org-p0-security-ekline@ekline.io>
2025-09-13 11:43:29 +0545

    Updated wbesite

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,7 +7,7 @@ export default async function sitemap() {
   const includeLocalePrefix = locales.length > 1;
 
   let blogs = locales.flatMap((locale) =>
-    getPosts(["src", "app", "[locale]", "blog", "posts", locale]).map(
+    getPosts(["src", "app", "[locale]", locale]).map(
       (post) => ({
         url: `${baseURL}${includeLocalePrefix ? `/${locale}` : ""}/blog/${
           post.slug
